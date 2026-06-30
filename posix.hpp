367a68effb4fb43cd2f28af992462418239ad9c6
Water Chika <water_chika@outlook.com>
2026-06-30 17:48:10 +0800

    fix compile error

--- posix.hpp
+++ posix.hpp
@@ -1,28 +1,32 @@
 #include <poll.h>
 
+#include <system_error>
+
 namespace posix {
 
 template<typename T>
 class add_poll_events : public T {
 public:
     using parent = T;
-    template<int FDS_SIZE>
-    void process_events(auto& fds) {
-        if (fds[FD_INDEX].revents) {
-            parent::process_event<FD_INDEX>(fds[FD_INDEX].revents); // dispatch event according to index.
-        }
-        process_events<FDS_SIZE-1>(fds);
-    }
-    template<>
-    void process_events<0>(auto& fds) {
-    }
     void poll_events() {
         auto fds = parent::get_fds(); // fds should be a array: meaning it has constexpr array size.
-        int ret = poll(fds.data(), fds.size(), -1);
+        int ret = poll(fds.data(), fds.size(), 100);
         if (ret == -1) {
-            throw std::system_error(errno, std::error_category::system_category);
+            throw std::system_error(errno, std::system_category());
         }
-        process_events<fds.size()>(fds);
+        parent::process_events(fds);
+    }
+};
+
+template<typename T>
+class add_empty_pollfd_array : public T {
+public:
+    using parent = T;
+    static constexpr int FDS_SIZE = 0;
+    void process_events(auto& fds) {
+    }
+    std::vector<pollfd> get_fds() {
+        return {};
     }
 };
 
